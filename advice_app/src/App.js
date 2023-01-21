7fa36109e566481d08f38715f1e8f32b8105d370
SANDESHSOBARAD <sobaradsandesh@gmail.com>
2023-01-21 15:16:27 -0800

    embedded the advice api

---
+++ advice_app/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './App.css';
+
+class App extends Component{
+
+  state = {advice: ''};
+
+  componentDidMount(){
+    this.fetchAdvice();
+    
+  }
+
+  fetchAdvice =()=>{
+    axios.get('https://api.adviceslip.com/advice')
+    .then((response)=> {
+      const {advice} = response.data.slip
+      console.log(advice);
+      this.setState({advice})
+    })
+    .catch((error)=> {
+      console.log(error)
+    })
+  }
+  render(){
+
+    return (
+      <div className='app'>
+              <div className='card'>
+              <h1 className='heading'>{this.state.advice}</h1>
+              </div>
+
+      </div>
+    )
+  }
+}
+
+export default App;
